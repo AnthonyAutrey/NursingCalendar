c6898eb1f8cc6aa4ad0e41ac3b7f2da3c56615df
Tony Autrey <autrey.anthony@gmail.com>
2018-04-22 00:24:30 -0500

    Formatting changes.

--- src/frontend/src/Administration/RoomResource.tsx
+++ src/frontend/src/Administration/RoomResource.tsx
@@ -21,44 +21,29 @@ export class RoomResource extends React.Component<Props, {}> {
 			return (<option key={uuid()} value={this.props.index}>{resource.name}</option>);
 		});
 
-		if (!this.props.selectedResource.count)
-			return (
-				<div className="d-flex mb-3">
-					<select
-						key={uuid()}
-						className="form-control"
-						value={this.props.selectedResource.name}
-						onChange={(event) => this.props.handleChangeResource(event, this.props.index)}
-					>
-						{resourceOptions}
-					</select>
-					<button className="btn btn-danger ml-2" onClick={() => this.props.handleDelete(this.props.index)}>&#10006;</button>
-				</div>
-			);
-		else
-			return (
-				<div className="d-flex mb-3">
-					<div className="col-lg-8 d-flex mr-3">
-						<select
-							key={uuid()}
-							className="form-control"
-							value={this.props.selectedResource.name}
-							onChange={(event) => this.props.handleChangeResource(event, this.props.index)}
-						>
-							{resourceOptions}
-						</select>
-					</div>
-					<div className="col-lg-4 d-flex">
-						<input
-							className="form-control"
-							type="number"
-							value={this.props.selectedResource.count}
-							onChange={(event) => this.props.handleChangeResourceCount(event, this.props.index)}
-						/>
-						<button className="btn btn-danger ml-2" onClick={() => this.props.handleDelete(this.props.index)}>&#10006;</button>
-					</div>
-				</div>
-			);
+		return (
+			<div className="d-flex mb-3">
+				<select
+					key={uuid()}
+					className="form-control"
+					value={this.props.selectedResource.name}
+					onChange={(event) => this.props.handleChangeResource(event, this.props.index)}
+				>
+					{resourceOptions}
+				</select>
+				{
+					this.props.selectedResource.count &&
+					<input
+						className="form-control ml-2"
+						style={{ width: 90 }}
+						type="number"
+						value={this.props.selectedResource.count}
+						onChange={(event) => this.props.handleChangeResourceCount(event, this.props.index)}
+					/>
+				}
+				<button className="btn btn-danger ml-2" onClick={() => this.props.handleDelete(this.props.index)}>&#10006;</button>
+			</div>
+		);
 	}
 }
 
